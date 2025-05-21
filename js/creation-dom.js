9ce130c953ea2b8c8aaadb5104b2ede0d34c5843
unknown <nduflot@dawan.fr>
2025-05-21 09:43:47 +0200

    créer dans le DOM pt 2 exo addspan

--- js/creation-dom.js
+++ js/creation-dom.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', function(){
         */
         console.log(event.target.dataset.col, event.target.parentNode.dataset.line);
         /*
-        pour créer un élément qui soit directement configurable dans le DOM, on utilise laméthode .createElement() de l'objet document
+        pour créer un élément qui soit directement configurable dans le DOM, on utilise la méthode .createElement() de l'objet document
         */
         const span = document.createElement('span');
         /*
@@ -51,4 +51,50 @@ window.addEventListener('DOMContentLoaded', function(){
         event.target.append(span);
         /*console.log(span);*/
     });
+
+    /*
+    Stop Propagation : 
+    quand un événement arrive sur un élément, certains événements "remontent" (bubbling) aux éléments parents
+    */
+
+    /* on surveille le clic sur la div stop01 et sur le bouton à l'intérieur sans arrêter le bubbling */
+    document.getElementById('stopP01').addEventListener('click', function(){
+        console.log('stopP01');
+    });
+
+    document.querySelector('#stopP01 button').addEventListener('click', function(){
+        console.log('button stopP01');
+    });
+    
+    /* on surveille le clic sur la div stopP02 et sur le bouton à l'intérieur en arrêtant la propagation du clic */
+    document.getElementById('stopP02').addEventListener('click', function(){
+        console.log('stopP02');
+    });
+    
+    document.querySelector('#stopP02 button').addEventListener('click', function(event){
+        event.stopPropagation();
+        console.log('button stopP02');
+    });
+
+    /*
+    preventDefault : quand un événement survient, il implique parfois une réponse du navigateur : 
+    - cliquer sur un lien fait ouvrir la source du lien
+    - cliquer sur un boutton de soumission de formulaire enclenche la tentative d'envoi du formulaire
+    preventdefault va permettre d'empêcher ce comportement, de pouvoir exécuter du script (vérification, lancement de téléchargement, appel ajax, etc) et js nous permet ensuite de revenir au comportement par défaut si besoin est
+    */
+
+    /* repérer le clic sur les liens ayant la classe testPreventLink */
+    document.querySelectorAll('.testPreventLink').forEach(function(a){
+        a.addEventListener('click', function(event){
+            event.preventDefault();
+            console.log(this.getAttribute('id'));
+        });
+    });
+
+    /*
+    guetter le clic sur le bouton addSpan, créer une span avec du texte dedans et l'ajouter au paragraphe avec l'id test
+    */
+
+    
+
 });
